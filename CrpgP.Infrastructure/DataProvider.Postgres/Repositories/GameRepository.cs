c4f2a3e0feade9f9fe25dce6c9ebb284ca01eac5
PorkyOrk <PorkyOrk@LAPTOP-OCOK20EP>
2023-12-02 23:35:27 +0100

    Updated queries with Dapper multi-mapping, for returning size data within the game object

--- CrpgP.Infrastructure/DataProvider.Postgres/Repositories/GameRepository.cs
+++ CrpgP.Infrastructure/DataProvider.Postgres/Repositories/GameRepository.cs
@@ -15,43 +15,74 @@ public class GameRepository : RepositoryBase, IGameRepository
     public async Task<Game?> FindByIdAsync(int gameId)
     {
         await using var cnn = await DataSource.OpenConnectionAsync();
-        const string sql = "SELECT * FROM games WHERE id = @GameId;";
-        return await cnn.QueryFirstOrDefaultAsync<Game>(sql, new
-        {
-            GameId = gameId
-        });
+
+        const string sql =
+            "SELECT * FROM games " +
+            "LEFT JOIN sizes ON games.size_id=sizes.id " +
+            "WHERE games.id = @GameId;";
+        
+        return cnn.QueryAsync<Game,Size,Game>(sql, (game, size) =>
+            {
+                game.PortraitSize = size; return game;
+            }, new
+            {
+                GameId = gameId
+            })
+            .GetAwaiter()
+            .GetResult()
+            .FirstOrDefault();
     }
     
     public async Task<Game?> FindByNameAsync(string gameName)
     {
         await using var cnn = await DataSource.OpenConnectionAsync();
-        const string sql = "SELECT * FROM games WHERE name = @GameName;";
-        return await cnn.QueryFirstOrDefaultAsync<Game>(sql, new
-        {
-            GameName = gameName
-        });
+        const string sql = 
+            "SELECT * FROM games " +
+            "LEFT JOIN sizes ON games.size_id=sizes.id " +
+            "WHERE name = @GameName;";
+        
+        return cnn.QueryAsync<Game,Size,Game>(sql, (game, size) =>
+            {
+                game.PortraitSize = size; return game;
+            }, new
+            {
+                GameName = gameName
+            })
+            .GetAwaiter()
+            .GetResult()
+            .FirstOrDefault();
     }
     
     public async Task<int> InsertAsync(Game game)
     {
         await using var conn = await DataSource.OpenConnectionAsync();
         const string sql =
-            "INSERT INTO games (name) VALUES (@GameName);" +
+            "INSERT INTO games (name, size_id) VALUES (@GameName, @SizeId);" +
             "SELECT currval('games_id_seq');";
-        return await conn.QuerySingleOrDefaultAsync<int>(sql, new
-        {
-            GameName = game.Name
-        });
+        
+        return conn.QueryAsync<int>(sql, new
+            {
+                GameName = game.Name,
+                SizeId = game.PortraitSize.Id
+            })
+            .GetAwaiter()
+            .GetResult()
+            .FirstOrDefault();
     }
     
     public async Task UpdateAsync(Game game)
     {
         await using var conn = await DataSource.OpenConnectionAsync();
-        const string sql = "UPDATE games SET name = @GameName WHERE id = @GameId;";
-        await conn.QuerySingleAsync(sql, new
+        const string sql = 
+            "UPDATE games " +
+            "SET name = @GameName, size_id = @SizeId " +
+            "WHERE id = @GameId;";
+        
+        await conn.QueryAsync(sql, new
         {
             GameName = game.Name,
-            GameId = game.Id
+            SizeId = game.PortraitSize.Id,
+            GameId = game.Id,
         });
     }
 
@@ -59,7 +90,7 @@ public class GameRepository : RepositoryBase, IGameRepository
     {
         await using var conn = await DataSource.OpenConnectionAsync();
         const string sql = "DELETE FROM games WHERE id = @GameId;";
-        await conn.QuerySingleAsync(sql, new
+        await conn.QueryAsync(sql, new
         {
             GameId = gameId
         });
